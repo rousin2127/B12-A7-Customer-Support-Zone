b888effc95ad9460246f5ea74aa16726935201b5
rousin2127 <rousinmd@gmail.com>
2025-09-27 10:56:21 +0600

    Add Navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-
-
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Count from "./components/Count"
 function App() {
 
   return (
-    <>
-      <h2 className="font-bold">Hello</h2>
-    </>
+    <div>
+      <Navbar />
+      <Count></Count>
+      <main> 
+        
+        <div className="w-[1020px] mx-auto flex">
+        <div className="w-[720px]">
+            <div><h2>Customer Tickets</h2></div>
+            <div>
+              
+            </div>
+          </div>
+          <div>
+            <div>
+              <h2>Task Status</h2>
+            </div>
+            <div>Resolved Task</div>
+          </div>
+        </div>
+      </main>
+      <Footer />  
+    </div>
+
   )
 }
 
---
+++ src/components/Count.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Count = () => {
+    return (
+        <div>
+            <div className="">
+                <div className="grid grid-cols-2 gap-6 my-[50px] w-[1020px] mx-auto">
+                    <div className="bg-purple-500 h-[200px] rounded-2xl flex flex-col justify-center items-center ">
+                        <h3 className="text-white">In Progress</h3>
+                        <span className="font-bold text-3xl text-white">0</span>
+                    </div>
+                    <div className="bg-green-500 h-[200px] rounded-2xl">
+                        <h3>Resolve</h3>
+                        <span>0</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Count;
